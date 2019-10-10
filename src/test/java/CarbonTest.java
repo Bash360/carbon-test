32cd310eb19817f0832f7a22ffd2e6fe48cc436a
mark bashir <beejayphil@gmail.com>
2019-10-10 13:34:43 +0100

    added test methods for test automation

--- src/test/java/CarbonTest.java
+++ src/test/java/CarbonTest.java
@@ -1,2 +1,20 @@
+import ng.com.carbon.CarbonAutomation;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 public class CarbonTest {
+    CarbonAutomation carbonAutomation;
+    @BeforeTest public void initAndroidDriver(){
+         carbonAutomation=new CarbonAutomation("Android","bash","9","/Users/bash/desktop/paylater-full-debug-v5.5.3-1909091739.apk");
+
+    }
+    @Test(priority = 1) public void signIn(){
+
+    }
+    @Test(priority = 2,dependsOnMethods = {"signIn"}) public void fundWallet(){
+
+    }
+    @Test(priority = 3,dependsOnMethods = {"fundWallet"}) public void checkTransaction(){
+
+    }
 }
